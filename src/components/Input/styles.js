753093c64dadc6fb1d01f646123ecf1acf2b8f23
Martina <martinanascimento95@gmail.com>
2024-07-22 10:06:48 -0300

    desafio calculadora

---
+++ src/components/Input/styles.js
@@ -0,0 +1,31 @@
+import styled from 'styled-components';
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 85%;
+    background-color: #AAAAFF;
+
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size: 24px;
+    font-family: 'Roboto', sans-serif;
+    
+
+    input{
+        width: 100%;
+        height:75px;
+        background-color: #AAAAFF;
+        border:0;
+        display: flex;
+        align-items: center;
+        justify-content: flex-end;
+        padding: 0 10px;
+
+        font-size: 24px;
+        font-family: 'Roboto', sans-serif;
+        color: #000;
+        
+    }
+`
